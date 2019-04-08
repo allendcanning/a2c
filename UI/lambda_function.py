8b9e8b31b55cfd4198a63daa98e5c6f0a18d50a8
Allen Canning <allen_canning@putnam.com>
2019-04-08 16:34:03 -0400

    Fixed syntax error

--- UI/lambda_function.py
+++ UI/lambda_function.py
@@ -193,6 +193,6 @@ def lambda_handler(event, context):
   return { 'statusCode': 200,
            'headers': {
               'Content-type': 'text/html'
-           }
+           },
            'body': content
-           }
+         }
