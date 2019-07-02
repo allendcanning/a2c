8fa84a8c83095c3aa00742789fb2b47b17737070
Allen Canning <allen_canning@putnam.com>
2019-07-02 18:15:39 -0400

    Added accept-charset to form attributes

--- athlete/lambda_function.py
+++ athlete/lambda_function.py
@@ -403,7 +403,7 @@ def edit_athlete_info(config,environment,record):
   policy = '{ "expiration": "'+str(t)+'", "conditions": [ {"acl": "bucket-owner-full-control" }, {"bucket": "a2c-transcripts-dev-530317771161-s3" }, {"x-amz-credential": "AKIAIOCUUZY3CYB4EGUA/20190702/us-east-1/s3/aws4_request" }, {"x-amz-server-side-encryption": "aws:kms"}, {"x-amz-algorithm": "AWS4-HMAC-SHA256"}, {"x-amz-date": "20190702T000000Z"} ] }'
   b64policy = base64.b64encode(bytes(policy,'UTF-8'))
 
-  user_record += '<form method="post" action="https://'+config['transcript_s3_bucket']+'.s3.amazonaws.com/" enctype="multipart/form-data">\n'
+  user_record += '<form method="post" accept-charset="UTF-8" action="https://'+config['transcript_s3_bucket']+'.s3.amazonaws.com/" enctype="multipart/form-data">\n'
   user_record += '<input type="hidden" name="action" value="upload">\n'
   user_record += '  <table class="defTable">\n'
   user_record += '    <tr><td class="header">Unofficial Transcripts: <td class="athletedata">'
