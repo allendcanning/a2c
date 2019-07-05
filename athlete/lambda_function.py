ab114175dd960c969f69c2c4b5ada1fe0e90d595
Allen Canning <allen_canning@putnam.com>
2019-07-04 21:11:37 -0400

    Added check for no contents

--- athlete/lambda_function.py
+++ athlete/lambda_function.py
@@ -421,9 +421,9 @@ def get_transcripts(config,username):
   # List objects from username folder
   try:
     response = client.list_objects_v2(Bucket=config['transcript_s3_bucket'], Prefix=username+'/*')
-    log_error("Response = "+json.dumps(response))
-    transcripts = response['Contents']
-    log_error("Transcripts = "+json.dumps(transcripts))
+    if 'Contents' in transcripts:
+      transcripts = response['Contents']
+      log_error("Transcripts = "+json.dumps(transcripts))
   except ClientError as e:
     log_error("response = "+json.dumps(e.response))
     log_error("Error is "+e.response['Error']['Message'])
